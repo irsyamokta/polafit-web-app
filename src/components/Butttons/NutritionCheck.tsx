d794d749ba7b0b22fdc250af20212c685b794099
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-02-03 23:48:23 +0700

    Add : Components button

---
+++ src/components/Butttons/NutritionCheck.tsx
@@ -0,0 +1,9 @@
+const ButtonNutritionCheck = () => {
+    return (
+        <button className="py-3 px-5 sm:ms-4 text-sm text-center font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary focus:z-10 focus:ring-4 focus:ring-gray-100">
+            Cek Nutrisi Makanan
+        </button>
+    )
+}
+
+export default ButtonNutritionCheck
