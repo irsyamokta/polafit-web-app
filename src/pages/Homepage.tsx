539ce5332f2aa1cda221843bcf6fc5e7ed12746b
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-02-12 23:40:26 +0700

    Add : Route path and add padding

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -3,12 +3,12 @@ import ButtonRecomendation from "../components/Butttons/Recomendation"
 import ButtonNutritionCheck from "../components/Butttons/NutritionCheck"
 const Homepage = () => {
     return (
-        <section className="h-[100vh] flex flex-col justify-center items-center lg:flex-row py-16 md:py-56 lg:py-20">
+        <section className="h-[100vh] flex flex-col justify-center items-center lg:flex-row py-16 md:py-56 lg:py-20 px-8">
             <div className="py-8 mx-auto">
                 <h1 className="mb-8 text-3xl text-center lg:text-start font-bold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl 2xl:text-7xl">Bantu Kamu Sehat dengan Pola yang Tepat</h1>
                 <p className="mb-8 text-base text-center font-normal text-gray-500 md:text-lg lg:text-xl lg:text-start">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro atque consectetur ipsa veniam eveniet! Minus numquam repellat sit, commodi facere deserunt et accusamus! Est libero aliquam maiores ipsa corporis iste!</p>
                 <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center lg:justify-start sm:space-y-0">
-                    <Link to="/">
+                    <Link to="/rekomendasi-olahraga">
                         <ButtonRecomendation />
                     </Link>
                     <Link to="/">
