54f93b8680193aa7fc53111d6d214d1799eb36f6
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-02-12 23:39:07 +0700

    Add : Exercise result

---
+++ src/components/Partials/ExerciseResult.tsx
@@ -0,0 +1,12 @@
+const ExerciseResult = () => {
+    return (
+        <div className="flex flex-col justify-center items-center gap-4 sm:w-1/2 px-8">
+            <h1 className="text-2xl font-semibold mb-5">Rekomendasi Olahraga Anda</h1>
+            <img src="src/assets/result/img-bersepeda.webp" alt="img result" className="lg:w-1/2 rounded-2xl"/>
+            <h1 className="text-4xl font-bold">Bersepeda</h1>
+            <p className="text-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur in consequuntur temporibus culpa voluptate ipsam dicta ea harum placeat molestiae.</p>
+        </div>
+    )
+}
+
+export default ExerciseResult
