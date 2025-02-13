22dea2c1d8090af4c47f9659e06e3f1cf3bfe766
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-02-14 00:04:49 +0700

    Update : Add props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { Routes, Route } from 'react-router'
-import Navbar from "./components/Partials/Navbar.tsx"
 import Homepage from "./pages/Homepage.tsx"
 import ExerciseRecomendation from './pages/ExerciseRecomendation.tsx'
 function App() {
   return (
     <>
-      <Navbar />
       <Routes>
         <Route path='/' element={<Homepage/>}/>
         <Route path='/rekomendasi-olahraga' element={<ExerciseRecomendation/>}/>
--- src/pages/ExerciseRecomendation.tsx
+++ src/pages/ExerciseRecomendation.tsx
@@ -1,14 +1,21 @@
 import ExerciseForm from "../components/Forms/ExerciseForm"
 import ExerciseResult from "../components/Partials/ExerciseResult"
+import useFormHandlers from '../hooks/useFormHandlers';
 
 const ExerciseRecomendation = () => {
+    const { formData, handleChange, handleSubmit, result } = useFormHandlers();
+
     return (
         <section className="flex flex-col lg:flex-row lg:h-[100vh] justify-center items-center py-16 gap-5">
             <div className="flex flex-col justify-center items-center bg-primary w-full lg:w-1/2 h-screen gap-5">
                 <h1 className="text-white text-2xl font-semibold mb-10">Lengkapi Data</h1>
-                <ExerciseForm />
+                <ExerciseForm 
+                    formData={formData} 
+                    handleChange={handleChange} 
+                    handleSubmit={handleSubmit} 
+                />
             </div>
-            <ExerciseResult />
+            {result && <ExerciseResult result={result} />}
         </section>
     )
 }
