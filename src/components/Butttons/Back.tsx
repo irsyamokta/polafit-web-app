4e6b901fe953d67b6c01d92fa452c5bc40ae047a
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-02-19 23:32:54 +0700

    Add : Back button

---
+++ src/components/Butttons/Back.tsx
@@ -0,0 +1,9 @@
+const Back = () => {
+    return (
+        <a href="/" type="button" className="text-white bg-transparent border border-white hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center">
+            Kembali
+        </a>
+    );
+}
+
+export default Back;
