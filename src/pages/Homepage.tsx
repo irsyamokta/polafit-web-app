cd047c90b3288d9f8aec101d4b23a9bf0cddb6c6
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-02-19 23:33:41 +0700

    Fix : Homepage

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -6,10 +6,12 @@ const Homepage = () => {
     return (
         <>
             <Navbar />
-            <section className="h-[100vh] flex flex-col justify-center items-center lg:flex-row py-16 md:py-56 lg:py-20 px-8">
+            <section className="h-[100vh] flex flex-col justify-center items-center lg:flex-row py-16 md:py-56 lg:py-20 px-4">
                 <div className="py-8 mx-auto">
-                    <h1 className="mb-8 text-3xl text-center lg:text-start font-bold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl 2xl:text-7xl">Bantu Kamu Sehat dengan Pola yang Tepat</h1>
-                    <p className="mb-8 text-base text-center font-normal text-gray-500 md:text-lg lg:text-xl lg:text-start">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro atque consectetur ipsa veniam eveniet! Minus numquam repellat sit, commodi facere deserunt et accusamus! Est libero aliquam maiores ipsa corporis iste!</p>
+                    <h1 className="mb-8 text-3xl font-bold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl 2xl:text-7xl">Bantu Kamu Sehat dengan Pola yang Tepat</h1>
+                    <p className="mb-8 text-base font-normal text-gray-500 md:text-lg lg:text-xl">
+                        Dapatkan rekomendasi olahraga dan pola makan yang sesuai dengan kebutuhan tubuhmu. Kami membantumu menemukan keseimbangan yang tepat antara aktivitas fisik dan asupan nutrisi untuk mencapai gaya hidup sehat secara optimal.
+                    </p>
                     <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center lg:justify-start sm:space-y-0">
                         <Link to="/rekomendasi-olahraga">
                             <ButtonRecomendation />
@@ -20,7 +22,7 @@ const Homepage = () => {
                     </div>
                 </div>
                 <img src="/src/assets/img-hero.png" className="w-2/3 lg:w-2/5" alt="healthy food" />
-                <img src="/src/assets/img-element.png" className="absolute bottom-0  -z-10" alt="wave" />
+                <img src="/src/assets/img-element.png" className=" absolute bottom-0 right-0 h-28 w-full lg:h-auto -z-20" alt="wave" />
             </section>
         </>
     )
