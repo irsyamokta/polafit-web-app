a2b6bd91639903f7492a949d27747dc1702d7082
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-02-19 22:40:42 +0700

    Add : No result component

---
+++ src/components/Partials/NoResult.tsx
@@ -0,0 +1,10 @@
+const NoResult = () => {
+    return (
+        <div className="flex flex-col justify-center items-center gap-4 sm:w-1/2 px-8">
+            <h1 className="text-2xl font-semibold mb-5">Hasil Akan Tampil Disini</h1>
+            <img src="src/assets/img-no-result.png" alt="img no result" />
+        </div>
+    )
+}
+
+export default NoResult;
