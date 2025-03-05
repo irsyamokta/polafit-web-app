0e274d8373a4ee818b51b49e3d53e2dd6cdb21bb
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-03-05 14:13:03 +0700

    Add : New route path

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router'
 import Homepage from "./pages/Homepage.tsx"
 import ExerciseRecomendation from './pages/ExerciseRecomendation.tsx'
+import NutritionPrediction from './pages/NutritionPrediction.tsx'
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<Homepage/>}/>
         <Route path='/rekomendasi-olahraga' element={<ExerciseRecomendation/>}/>
+        <Route path='/cek-nutrisi' element={<NutritionPrediction/>}/>
       </Routes>
     </>
   )
